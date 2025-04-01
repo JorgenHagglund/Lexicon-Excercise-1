2d8d538a9ac0c98d615b2b59aca6515a8fad4e42
Jörgen Hägglund <jorgen@sirlucifer.mine.nu>
2025-04-01 14:36:39 +0200

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise_1;
+
+public class Employee
+{
+    string firstName { get; set; }  
+    string lastName { get; set; }       
+    float salary { get; set; }
+
+    public Employee(string firstName, string lastName, float salary)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+    }   
+
+    public Employee(string firstName, float salary)
+    {
+        this.firstName = firstName;
+        this.lastName = string.Empty;
+        this.salary = salary;
+    }  
+
+    public void Print()
+    {
+        Console.WriteLine($"{firstName}\t{lastName}\t{salary:F2}");  
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Excercise_1;
+
+internal class Program
+{
+    private static List<Employee> employees = new();
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("The small restaurant");
+        Console.WriteLine("====================");
+        Console.WriteLine("Please enter your employees as <first name>[tab]<last name>[tab]<salary>");
+        Console.WriteLine("or <first name>[tab]<salary>, end with an empty line.");
+
+        while (true)    
+        {
+            string input = Console.ReadLine();
+            if (string.IsNullOrEmpty(input))
+            {
+                break;
+            }
+            var parts = input.Split('\t');
+
+            Employee? employee;
+            if (parts.Length == 2)
+            {
+                employee = new Employee(parts[0], float.Parse(parts[1]));
+            }
+            else if (parts.Length > 2)
+            {
+                employee = new Employee(parts[0], parts[1], float.Parse(parts[2]));
+            } 
+            else
+            {
+                Console.WriteLine("Invalid input");
+                continue;
+            }
+            if (employee is not null)
+            {
+                employees.Add(employee);
+            }
+        }
+
+        Console.WriteLine();    
+        Console.WriteLine($"Employees ({employees.Count()}):");
+        foreach (var employee in employees)
+        {
+            employee.Print();
+        }
+    }
+}
